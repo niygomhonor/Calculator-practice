cf90ece153246b331ad1d1f24c141637c18f5381
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-07-15 18:57:44 +0200

    Add arthimetic operators and condition

--- Calculator-js/calculator.js
+++ Calculator-js/calculator.js
@@ -4,4 +4,14 @@ var add = function(number1, number2) {
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
-  alert(add(number1, number2));
+  var result = add(number1, number2);
+  alert(result);
+  var subtract = function(number1, number2) {
+   
+    if (number1>number2) {
+        return number1 - number2; 
+    }
+    else{
+        return number2 -number1;
+    }
+  };
