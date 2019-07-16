800ca45d43eac3c72ff9e80de9bbf11a87e76e79
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-07-16 08:55:23 +0200

    Add switch case

--- Calculator-js/calculator.js
+++ Calculator-js/calculator.js
@@ -1,12 +1,14 @@
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+// Addition
+
 var add = function(number1, number2) {
     return number1 + number2;
   };
-  
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = add(number1, number2);
-  alert(result);
-  var subtract = function(number1, number2) {
+// Subtraction
+
+var subtract = function(number1, number2) {
    
     if (number1>number2) {
         return number1 - number2; 
@@ -15,3 +17,38 @@ var add = function(number1, number2) {
         return number2 -number1;
     }
   };
+
+  // Multiplcation
+
+  var multiply = function(number1, number2) {
+    return number1 + number2;
+  };
+
+  //Division
+  var divide = function(number1, number2) {
+    return number1 + number2;
+  };
+   
+  switch (operator) {
+      case "sum":
+      var result = add(number1, number2);
+      alert(result);
+          break;
+  
+          case "difference":
+          var result = subtract(number1, number2);
+alert(result);
+          break;
+          case "product":
+          var result = multiply(number1, number2);
+alert(result);
+          break;
+          case "quotient":
+          var result = divide(number1, number2);
+alert(result);
+          break;
+      default:
+          break;
+  }
+
+
